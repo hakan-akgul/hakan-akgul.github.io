9db07b8f97232956e5d9bade2adb82dd6e131bdf
hakan-akgul <akgul.hakan.dev@gmail.com>
2024-04-07 14:12:44 +0300

    feat: :lipstick: moajor ui updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,52 +7,58 @@ const themeArmada = ExpressiveCodeTheme.fromJSONString(jsoncString)
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hakan-akgul.github.io/',
-	integrations: [
-		starlight({
-			tableOfContents: false,
-			title: 'HAKAN AKGÜL',
-			social: {
-				github: 'https://github.com/hakan-akgul',
-				gitlab: 'https://gitlab.com/hakan-akgul',
-				linkedin: 'https://www.linkedin.com/in/hakanakgul',
-				stackOverflow: 'https://stackoverflow.com/users/13446430/hakan-akgul'
-			},
-			expressiveCode: {
-				themes: [ themeArmada ],
-				styleOverrides: {
-					borderColor: 'none',
-					borderWidth: '2px',
-					borderRadius: '4px',
-					codeBackground: 'var(--navy-d)',
-					focusBorder: 'var(--carnation)',
-					frames: {
-						editorActiveTabBackground: 'var(--navy-d)',
-						editorActiveTabBorderColor: 'none',
-						editorActiveTabForeground: 'var(--navy-lllll)',
-						editorActiveTabIndicatorBottomColor: 'none',
-						editorTabBarBackground: 'none',
-						editorTabBarBorderBottomColor: 'none',
-						editorTabBorderRadius: '4px',
-						terminalBackground: 'var(--navy-l)',
-						terminalTitlebarBorderBottomColor: 'none',
-						terminalTitlebarDotsForeground: 'var(--navy-l)',
-						terminalTitlebarDotsOpacity: '100%',
-						terminalTitlebarForeground: 'var(--navy-lll)',
+  site: 'https://hakan-akgul.github.io/',
+  base: '/docs',
+  trailingSlash: 'never',
+  integrations: [
+    starlight({
+      tableOfContents: false,
+      title: 'hakan akgül',
+      social: {
+        github: 'https://github.com/hakan-akgul',
+        gitlab: 'https://gitlab.com/hakan-akgul',
+        linkedin: 'https://www.linkedin.com/in/hakanakgul',
+        stackOverflow: 'https://stackoverflow.com/users/13446430/hakan-akgul'
+      },
+      expressiveCode: {
+        themes: [ themeArmada ],
+        styleOverrides: {
+          borderColor: 'none',
+          borderWidth: '2px',
+          borderRadius: '4px',
+          codeBackground: 'var(--navy-d)',
+          focusBorder: 'var(--carnation)',
+          frames: {
+            editorActiveTabBackground: 'var(--navy-d)',
+            editorActiveTabBorderColor: 'none',
+            editorActiveTabForeground: 'var(--navy-lllll)',
+            editorActiveTabIndicatorBottomColor: 'none',
+            editorTabBarBackground: 'none',
+            editorTabBarBorderBottomColor: 'none',
+            editorTabBorderRadius: '4px',
+            terminalBackground: 'var(--navy-d)',
+            terminalTitlebarBorderBottomColor: 'none',
+            terminalTitlebarDotsForeground: 'var(--navy-l)',
+            terminalTitlebarDotsOpacity: '100%',
+            terminalTitlebarForeground: 'var(--navy-lll)',
+            terminalTitlebarBackground: 'var(--navy)',
+            editorTabBarBorderColor: 'var(--navy-d)',
+            inlineButtonBackground: 'var(--carnation)',
+            inlineButtonBorder: 'var(--carnation)',
+            inlineButtonForeground: 'var(--carnation-lll)',
+          }
 
-					}
-
-				}
-			},
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				}
-			],
-		}),
-	],
+        }
+      },
+      customCss: [
+        './src/styles/custom.css',
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        }
+      ],
+    }),
+  ],
 });
--- src/content/docs/guides/welcome/welcome.md
+++ src/content/docs/guides/welcome/welcome.md
@@ -2,10 +2,7 @@
 title: Welcome
 ---
 
-:::tip[Hi!]
+##### I don't like the blogs that explain a problem in a long post.  
+##### Generally coders have a short time period for solving a problem.
 
-I don't like the blogs that explain a problem in a long post.  
-Generally coders have a short time period for solving a problem.
-
-Just copy the code and close your 40+ opened tabs!
-:::
+##### Just copy the code and close your 40+ opened tabs!
