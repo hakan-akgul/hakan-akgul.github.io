3c531efcfcb5c784d50e7ccfdd06a9180cb9a79c
hakan-akgul <akgul.hakan.dev@gmail.com>
2024-04-12 23:10:26 +0300

    feat: :heavy_plus_sign: markdown parser added for rss & dependency updates

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
-import { rssSchema } from '@astrojs/rss';
+import { docsSchema } from '@astrojs/starlight/schema';
 import { defineCollection } from 'astro:content';
 
-
 export const collections = {
-	docs: defineCollection({ schema: rssSchema }),
+	docs: defineCollection({ schema: docsSchema() }),
 };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,9 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const documents = await getCollection('docs');
@@ -14,7 +18,9 @@ export async function GET(context) {
       pubDate: document.data.lastUpdated || new Date('1989-03-07'),
       description: document.data.description,
       customData: document.data.customData,
-      content: document.body,
+      content: sanitizeHtml(parser.render(document.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])
+      }),
       author: 'hakan akgül',
       link: `https://hakan-akgul.github.io/${document.slug}`,
     })),
